33e91f8e1a0b0be9d11a74fa539400cce16ac572
NUS-ISS-Repo <62528384+NUS-ISS-Repo@users.noreply.github.com>
2022-09-06 18:56:43 +0800

    Initial Commit

---
+++ Models/VenueModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClassVenueInfo.Models
+{
+    public class VenueModels
+    {
+        public List<VenueModel> VenueModel { get; set; }
+    }
+
+    public class VenueModel
+    {
+        public string CourseCode { get; set; }
+        public string CourseDesc { get; set; }
+        public string ClassTime { get; set; }
+        public string ClassTimeCode { get; set; }
+        public string ClassDate { get; set; }
+        public string RoomCode { get; set; }
+        public string RoomType { get; set; }
+        public string Department { get; set; }
+    }
+}
